48f9ecc26940be066340f6b41444cfe571250edd
Charlotte0008 <113023238+charlottejruby@users.noreply.github.com>
2022-11-29 20:02:26 +0900

    average rate function change

--- database.py
+++ database.py
@@ -138,10 +138,10 @@ class DBhandler:
             value=res.val()
             if value['name']==name:
                 rates.append(float(value['rating']))
-                if len(rates) < 0:
-                    return 0
-                else :
-                    return sum(rates)/len(rates)
+        if len(rates) <= 0:
+            return 0
+        else :
+            return sum(rates)/len(rates)
 
     
         
